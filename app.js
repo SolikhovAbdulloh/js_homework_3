d54143659bebfd7844584bb6429fa32a2fe13769
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-10 16:33:11 +0500

    js_homework

---
+++ app.js
@@ -0,0 +1,238 @@
+// 11-masala
+
+// let arr = [8,8,33,42,5,'abd']
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     console.log(arr[i])
+// }
+
+// 12-masala
+
+// let arr = [8, 8, 33, 42, 5, 1];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2==0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// 13-masala
+
+// let arr = [8, 8, 33, 42, 5, 1];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2==1) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// 14-masala
+
+// let arr = [8, 8, 33, 42, 5, 1];
+
+// let max = arr[0] ;
+
+// for (let i = 0; i < arr.length; i++){
+//     if(arr[i] > max){
+//         max=arr[i]
+//     }
+// }
+// console.log(max)
+
+// 15-masala
+
+// let arr = [8, 8, 33, 42, 5, 1,-4];
+
+// let min = arr[0] ;
+
+// for (let i = 0; i < arr.length; i++){
+//     if(arr[i] < min){
+//         min = arr[i]
+//     }
+// }
+// console.log(min)
+
+// 16-masala
+
+// let arr = [8, 8, 3, 2, 5, 1];
+
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++){
+//     sum+=arr[i]
+// }
+// console.log(sum)
+
+// 17-masala
+
+// let arr = [2,-3,4,-4,7,8,-1]
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     if(arr[i] > 0){
+//         console.log(arr[i])
+//     }
+// }
+
+// 18-masala
+
+// let arr = [2,-3,4,-4,7,8,-1]
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     if(arr[i] < 0){
+//         console.log(arr[i])
+//     }
+// }
+
+// 19-masala
+
+// let arr = [2,3,4,5,6,7]
+
+// let array = []
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     array += arr[i]*2
+// }
+// console.log(array)
+
+// 20-masala
+
+// let arr = [1,2,3,4,5,6]
+
+// for(let i = 0 ; i < arr.length ;i++){
+//     if(arr[i]%2==1){
+//         arr.splice(i,1)
+//     }
+// }
+// console.log(arr)
+
+// 21-masala
+
+// let str = 'Abdulloh'
+
+// for(let i = 0 ; i < str.length ; i++){
+//     console.log(str[i])
+// }
+
+// 22-masala
+
+// let str = "solikHOV"
+
+// count = 0
+
+// for(let i = 0 ; i < str.length ; i++){
+//     if(str[i] >= 'A' && str[i]<'Z'){
+//         count++
+//     }
+// }
+// console.log(count + " ta katta harf bor")
+
+// 23-masala
+
+// let str = "SOLIkhovc"
+
+// count = 0
+
+// for(let i = 0 ; i < str.length ; i++){
+//     if(str[i] >= 'a' && str[i]<'z'){
+//         count++
+//     }
+// }
+// console.log(count + " ta kichik harf bor")
+
+// 24-masala
+
+// let str = 'Apple701'
+
+// count = 0
+
+// for(let i = 0 ; i < str.length ; i++){
+//     if(!isNaN(str[i])){
+//         count++
+//     }
+// }
+// console.log(count + ' ta raqam bor')
+
+// 25-masala
+
+// let str = 'solikhov abdulloh 26-yosh 1999-yil'
+
+// count = 0
+
+// for (let i = 0 ; i < str.length; i++){
+//     if(str[i]==" "){
+//         count++
+
+//     }
+// }
+// console.log(count)
+
+// 26-masala
+
+// let str = 'helloworld'
+
+// for(let i = str.length-1 ; i >= 0 ; i--){
+//         console.log(str[i])
+// }
+
+// 27-masala
+
+// let str = 'AAAaaabbbweweqw'
+
+// let  count = 0;
+
+// for(let i = 0 ; i < str.length ; i++){
+//     if(str[i]==='a'){
+//         count++
+//     }
+// }
+// console.log(count)
+
+// 28-masala
+
+// let str = 'helloworld'
+
+// for(let i = 0 ; i < str.length ; i++){
+//     console.log(str[i] + "\n" )
+// }
+
+// 30-masala
+
+// let str = "a1238dasaf";
+
+// let sum = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//   if (!isNaN(str[i])) {
+//     sum += Number(str[i]);
+//   }
+// }
+// console.log(sum);
+
+// 31-masala
+
+// let a = prompt("son kiriting")
+// let a = 241
+
+// let yuzlar = Math.floor(a/100)
+
+// let onlik = Math.floor(a%100/10);
+
+// let birlik = a%10
+
+// let sum = yuzlar + onlik + birlik
+
+// console.log(" yigindis " + sum);
+
+
+// 32 - masala 
+
+// let arr = [8, 8, 33, 42, 5, 1];
+
+// let max = arr[0] ;
+
+// for (let i = 0; i < arr.length; i++){
+//     if(arr[i] > max){
+//         max=arr[i]
+//     }
+// }
+// console.log(max)
